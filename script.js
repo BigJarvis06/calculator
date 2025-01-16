8dc079d96d1209aa1301d1f842470c16042f7699
BigJarvis06 <tjdunn06@gmail.com>
2025-01-16 19:55:55 +1100

    added math functions and operation variables

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// math functions
+
+function add(a, b) {
+	return a + b;
+}
+
+function subtract(a, b) {
+	return a - b;
+}
+
+function multiply(a, b) {
+	return a * b;
+}
+
+function divide(a, b) {
+	return a / b;
+}
+
+// operation variables
+
+let firstNum, secondNum, operator;
+
