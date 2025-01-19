df016b6bc4db26ab58de169213a5fc6801c5fe89
BigJarvis06 <tjdunn06@gmail.com>
2025-01-19 16:46:22 +1100

    completed calc?

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 // math functions
 
 const add = function(a, b) {
-	return a + b;
+	return Number(a + b);
 }
 
 const subtract = function(a, b) {
-	return a - b;
+	return Number(a - b);
 }
 
 const multiply = function(a, b) {
@@ -18,16 +18,16 @@ const divide = function(a, b) {
 }
 
 const inputs = {
-firstNum : "",
-secondNum : "",
-operator : undefined
+"firstNum" : "",
+"secondNum" : "",
+"operator" : undefined
 }
 // operate function
 
 function operate(a, b, op) {
-	a = firstNum;
-	b = secondNum;
-	op = operator;
+	a = Number(inputs.firstNum);
+	b = Number(inputs.secondNum);
+	op = inputs.operator;
 	switch (op) {
 		case add:
 				return add(a, b);
@@ -53,9 +53,29 @@ function operate(a, b, op) {
 const display = document.querySelector("#display");
 
 const changeDisplay = function(a) {
-	display.textContent += a;
+	switch (a) {
+		case buttons.add.textContent:
+		case buttons.subtract.textContent:
+		case buttons.multiply.textContent:
+		case buttons.divide.textContent:
+			display.textContent = a;
+			break;
+		case operate:
+			display.textContent = a;
+			break;
+		default:
+			if (display.textContent.includes(buttons.add.textContent) ||
+				display.textContent.includes(buttons.subtract.textContent) ||
+				display.textContent.includes(buttons.multiply.textContent) ||
+				display.textContent.includes(buttons.divide.textContent)) 
+				{
+				display.textContent = "";
+				display.textContent += a;
+			} else {
+				display.textContent += a;
+			}
+	}
 }
-
 const clearDisplay = function() {
 	display.textContent = "";
 }
@@ -83,6 +103,14 @@ const buttons = {
 
 // event listeners
 
+function checkOp(num) {
+	if (!inputs.operator) {
+		inputs.firstNum += num;
+	} else {
+		inputs.secondNum += num;
+	}
+}
+
 Object.keys(buttons).forEach(button => {
 	addEventListener("click", event => {
 		let target = event.target;
@@ -90,125 +118,93 @@ Object.keys(buttons).forEach(button => {
 // numbers
 			case "number-one":
 					changeDisplay("1");
-					if (!operator) {
-						inputs.firstNum + "1";
-					} else {
-						inputs.secondNum + "1";
-					}
+					checkOp("1");
 					event.stopImmediatePropagation();
 				break;
 			case "number-two":
 					changeDisplay("2");
-					if (!operator) {
-						firstNum = `${firstNum}` + "2";
-					} else {
-						secondNum = `${secondNum}` + "2";
-					}
+					checkOp("2");
 					event.stopImmediatePropagation();
 				break;
 			case "number-three":
 					changeDisplay("3");
-					if (!operator) {
-						firstNum = `${firstNum}` + "3";
-					} else {
-						secondNum = `${secondNum}` + "3";
-					}
+					checkOp("3");
 					event.stopImmediatePropagation();
 				break;
 			case "number-four":
 					changeDisplay("4");
-					if (!operator) {
-						firstNum = `${firstNum}` + "4";
-					} else {
-						secondNum = `${secondNum}` + "4";
-					}
+					checkOp("4");
 					event.stopImmediatePropagation();
 				break;
 			case "number-five":
 					changeDisplay("5");
-					if (!operator) {
-						firstNum = `${firstNum}` + "5";
-					} else {
-						secondNum = `${secondNum}` + "5";
-					}
+					checkOp("5");
 					event.stopImmediatePropagation();
 				break;
 			case "number-six":
 					changeDisplay("6");
-					if (!operator) {
-						firstNum = `${firstNum}` + "6";
-					} else {
-						secondNum = `${secondNum}` + "6";
-					}
+					checkOp("6");
 					event.stopImmediatePropagation();
 				break;
 			case "number-seven":
 					changeDisplay("7");
-					if (!operator) {
-						firstNum = `${firstNum}` + "7";
-					} else {
-						secondNum = `${secondNum}` + "7";
-					}
+					checkOp("7");
 					event.stopImmediatePropagation();
 				break;
 			case "number-eight":
 					changeDisplay("8");
-					if (!operator) {
-						firstNum = `${firstNum}` + "8";
-					} else {
-						secondNum = `${secondNum}` + "8";
-					}
+					checkOp("8");
 					event.stopImmediatePropagation();
 				break;
 			case "number-nine":
 					changeDisplay("9");
-					if (!operator) {
-						firstNum = `${firstNum}` + "9";
-					} else {
-						secondNum = `${secondNum}` + "9";
-					}
+					checkOp("9");
 					event.stopImmediatePropagation();
 				break;
 			case "number-zero":
 					changeDisplay("0");
-					if (!operator) {
-						firstNum = `${firstNum}` + "0";
-					} else {
-						secondNum = `${secondNum}` + "0";
-					}
+					checkOp("0");
 					event.stopImmediatePropagation();
 				break;
 // operations
 			case "clear":
 					clearDisplay();
-					firstNum = "";
-					secondNum = "";
-					operator = null;
+					inputs.firstNum = "";
+					inputs.secondNum = "";
+					inputs.operator = undefined;
 					event.stopImmediatePropagation();
 				break;
 			case "equals":
-					if (firstNum && secondNum && operator) {
-					operate();
-					event.stopImmediatePropagation();
-					} else {
+					if (inputs.firstNum && inputs.secondNum && inputs.operator) {
+						clearDisplay();
+						changeDisplay(operate());
+							inputs.firstNum = "";
+							inputs.secondNum = "";
+							inputs.operator = undefined;
+								event.stopImmediatePropagation();
+						} else {
 						break;
 					}
 				break;
 			case "add":
-					operator = add;
+					changeDisplay(buttons.add.textContent);
+					inputs.operator = add;
 					console.log("add")
 					event.stopImmediatePropagation();
 				break;
 			case "subtract":
-					operator = subtract();
+					changeDisplay(buttons.subtract.textContent);
+					inputs.operator = subtract;
 					event.stopImmediatePropagation();
 				break;
 			case "multiply":
-					operator = multiply();
+				changeDisplay(buttons.multiply.textContent);
+					inputs.operator = multiply;
 					event.stopImmediatePropagation();
 				break;
 			case "divide":
-					operator = divide();
+					changeDisplay(buttons.divide.textContent);
+					inputs.operator = divide;
 					event.stopImmediatePropagation();
 				break;
 		}
